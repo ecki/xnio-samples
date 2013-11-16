65b8293c8a61a791ba5fc6faaf74ce169ee4ab67
Bernd Eckenfels <bernd@eckenfels.net>
2013-11-16 15:30:59 +0100

    use 3.1 API

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 xnio-samples
 ============
 
-Sample code for XNIO 3.0/3.1
+Sample code for XNIO 3.1
 
 This work is based on the XNIO2 samples from David Lloyd on the XNIO Quick Start page on
 https://community.jboss.org/wiki/XNIOQuickStart.
@@ -10,8 +10,8 @@ https://community.jboss.org/wiki/XNIOQuickStart.
 Starting the samples
 --------------------
     C:\ws\github\org.xnio.samples>set JB=%USERPROFILE%\.m2\repository\org\jboss
-    C:\ws\github\org.xnio.samples>set CP     =%JB%\xnio\xnio-api\3.0.8.GA\xnio-api-3.0.8.GA.jar
-    C:\ws\github\org.xnio.samples>set CP=%CP%;%JB%\xnio\xnio-nio\3.0.8.GA\xnio-nio-3.0.8.GA.jar
+    C:\ws\github\org.xnio.samples>set CP     =%JB%\xnio\xnio-api\3.1.0.Final\xnio-api-3.1.0.Final.jar
+    C:\ws\github\org.xnio.samples>set CP=%CP%;%JB%\xnio\xnio-nio\3.1.0.Final\xnio-nio-3.1.0.Final.jar
     C:\ws\github\org.xnio.samples>set CP=%CP%;%JB%\logging\jboss-logging\3.1.2.GA\jboss-logging-3.1.2.GA.jar
     C:\ws\github\org.xnio.samples>set CP=%CP%;target\xnio-samples-0.0.1-SNAPSHOT.jar
     C:\ws\github\org.xnio.samples>"%JAVA_HOME%\bin\java" -cp %CP% org.xnio.samples.SimpleEchoServer
--- src/main/java/org/xnio/samples/SimpleHelloWorldBlockingClient.java
+++ src/main/java/org/xnio/samples/SimpleHelloWorldBlockingClient.java
@@ -8,10 +8,12 @@ import java.nio.charset.Charset;
 import org.xnio.IoFuture;
 import org.xnio.IoUtils;
 import org.xnio.OptionMap;
+import org.xnio.StreamConnection;
 import org.xnio.Xnio;
 import org.xnio.XnioWorker;
 import org.xnio.channels.Channels;
-import org.xnio.channels.ConnectedStreamChannel;
+import org.xnio.channels.StreamSinkChannel;
+import org.xnio.channels.StreamSourceChannel;
 
 public final class SimpleHelloWorldBlockingClient {
 
@@ -21,26 +23,30 @@ public final class SimpleHelloWorldBlockingClient {
         final XnioWorker worker = xnio.createWorker(OptionMap.EMPTY);
 
         try {
-            final IoFuture<ConnectedStreamChannel> futureConnection = worker.connectStream(new InetSocketAddress("localhost", 12345), null, OptionMap.EMPTY);
-            final ConnectedStreamChannel channel = futureConnection.get(); // throws exceptions
+            final IoFuture<StreamConnection> futureConnection = worker.openStreamConnection(new InetSocketAddress("localhost", 12345), null, OptionMap.EMPTY);
+            final StreamConnection connection = futureConnection.get(); // blocks and throws exceptions
+            final StreamSinkChannel outChannel = connection.getSinkChannel();
+            final StreamSourceChannel inChannel = connection.getSourceChannel();
             try {
                 // Send the greeting
-                Channels.writeBlocking(channel, ByteBuffer.wrap("Hello world!\n".getBytes(charset)));
+                Channels.writeBlocking(outChannel, ByteBuffer.wrap("Hello world!\n".getBytes(charset)));
                 // Make sure all data is written
-                Channels.flushBlocking(channel);
+                Channels.flushBlocking(outChannel);
                 // And send EOF
-                channel.shutdownWrites();
+                outChannel.shutdownWrites();
                 System.out.println("Sent greeting string!  The response is...");
                 ByteBuffer recvBuf = ByteBuffer.allocate(128);
                 // Now receive and print the whole response
-                while (Channels.readBlocking(channel, recvBuf) != -1) {
+                while (Channels.readBlocking(inChannel, recvBuf) != -1) {
                     recvBuf.flip();
                     final CharBuffer chars = charset.decode(recvBuf);
                     System.out.print(chars);
                     recvBuf.clear();
                 }
             } finally {
-                IoUtils.safeClose(channel);
+                IoUtils.safeClose(inChannel);
+                IoUtils.safeClose(outChannel);
+                IoUtils.safeClose(connection);
             }
         } finally {
             worker.shutdown();
